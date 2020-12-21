cd2fc68c2857c62d058fae883cab4267577ad344
Luiz Fernando de Santana <luizfernando.itpro@gmail.com>
2020-12-21 10:36:38 -0300

    link com as rotas dos componentes que vou utilizar

--- Angular11/src/app/app-routing.module.ts
+++ Angular11/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { UserInfoComponent } from './pages/user-info/user-info.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'userInfo', component: UserInfoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
